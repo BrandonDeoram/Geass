1c8896a845b0436a839501f4b25c5cfc424b75ee
BrandonDeoram <brandondeoram8931@gmail.com>
2022-04-20 09:25:54 -0400

    Added bottomBar

--- geassapp/lib/screens/cards/anime_card.dart
+++ geassapp/lib/screens/cards/anime_card.dart
@@ -1,10 +1,11 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:geassapp/models/anime.dart';
+import 'package:google_sign_in/testing.dart';
 import 'package:readmore/readmore.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 // ignore_for_file: prefer_const_constructors
 class AnimeCard extends StatelessWidget {
@@ -15,133 +16,161 @@ class AnimeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       backgroundColor: Theme.of(context).backgroundColor,
       body: SafeArea(
+          bottom: false,
           child: Stack(
-        children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.only(bottomRight: Radius.circular(50)),
-              child: Container(
-                height: 800.h,
-                width: 1200.w,
-                child: ImageFiltered(
-                  imageFilter: ImageFilter.blur(sigmaY: 2.8, sigmaX: 2.8),
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                child: ClipRRect(
+                  borderRadius:
+                      BorderRadius.only(bottomRight: Radius.circular(50)),
+                  child: Container(
+                    height: 800.h,
+                    width: 1200.w,
+                    child: ImageFiltered(
+                      imageFilter: ImageFilter.blur(sigmaY: 2.8, sigmaX: 2.8),
+                      child: Image(
+                        image: NetworkImage(anime.image),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(60, 0, 0, 0),
+                child: Container(
+                  height: 800.h,
+                  width: 800.w,
                   child: Image(
                     image: NetworkImage(anime.image),
                     fit: BoxFit.fill,
                   ),
                 ),
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(60, 0, 0, 0),
-            child: Container(
-              height: 800.h,
-              width: 800.w,
-              child: Image(
-                image: NetworkImage(anime.image),
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-          IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(
-                Icons.arrow_back,
-                color: Colors.white,
-              )),
-          Padding(
-            padding: EdgeInsets.fromLTRB(338, 255, 0, 0),
-            child: Container(
-              width: 50.0,
-              height: 50.0,
-              decoration: new BoxDecoration(
-                color: Colors.red,
-                shape: BoxShape.circle,
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(335, 253, 0, 0),
-            child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(
-                  Icons.favorite,
-                  color: Colors.white,
-                  size: 40.0,
-                )),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(10, 300, 0, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  anime.name,
-                  style: TextStyle(fontSize: 30),
+              IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  )),
+              Padding(
+                padding: EdgeInsets.fromLTRB(338, 255, 0, 0),
+                child: Container(
+                  width: 50.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
                 ),
-                Row(
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(335, 253, 0, 0),
+                child: IconButton(
+                    iconSize: 40.0,
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.favorite,
+                      color: Colors.white,
+                      size: 40.0,
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 300, 0, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    for (var i in anime.categories)
-                      Text(
-                        i.toString() + " ",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w200, color: Colors.grey),
-                      )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 5),
-                      child: Container(
-                        height: 20,
-                        child: RatingBarIndicator(
-                          rating: double.parse(anime.rating),
-                          itemBuilder: (context, index) => Icon(
-                            Icons.star,
-                            color: Colors.amber,
+                    Text(
+                      anime.name,
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Row(
+                      children: [
+                        for (var i in anime.categories)
+                          Text(
+                            i.toString() + " ",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w200,
+                                color: Colors.grey),
+                          )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Container(
+                            height: 20,
+                            child: RatingBarIndicator(
+                              rating: double.parse(anime.rating),
+                              itemBuilder: (context, index) => Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                              itemCount: 5,
+                              itemSize: 20.0,
+                              direction: Axis.horizontal,
+                            ),
                           ),
-                          itemCount: 5,
-                          itemSize: 20.0,
-                          direction: Axis.horizontal,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 8.0),
+                          child: Text(anime.rating),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10, right: 10),
+                      child: Container(
+                        height: 180,
+                        child: SingleChildScrollView(
+                          child: ReadMoreText(
+                            anime.descript,
+                            trimLines: 11,
+                            trimMode: TrimMode.Line,
+                            trimCollapsedText: '"',
+                            trimExpandedText: ' show less',
+                          ),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 8.0),
-                      child: Text(anime.rating),
-                    )
                   ],
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Container(
-                    height: 200,
-                    child: SingleChildScrollView(
-                      child: ReadMoreText(
-                        anime.descript,
-                        trimLines: 11,
-                        colorClickableText: Colors.pink,
-                        trimMode: TrimMode.Line,
-                        trimCollapsedText: '"',
-                        trimExpandedText: ' show less',
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
+              ),
+            ],
+          )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
+      floatingActionButton: SpeedDial(
+        overlayColor: Colors.transparent,
+        buttonSize: Size(50, 50),
+        icon: Icons.add,
+        activeIcon: Icons.close,
+        backgroundColor: Colors.white,
+        direction: SpeedDialDirection.right,
+        children: [
+          SpeedDialChild(
+              child: Icon(Icons.playlist_add_rounded),
+              onTap: () {
+                print('tapped');
+              }),
+          SpeedDialChild(child: Icon(Icons.remove_red_eye), onTap: () {}),
+          SpeedDialChild(child: Icon(Icons.done), onTap: () {}),
         ],
-      )),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white12,
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 14.0,
+        child: Container(
+          height: 30,
+        ),
+      ),
     );
   }
 }
