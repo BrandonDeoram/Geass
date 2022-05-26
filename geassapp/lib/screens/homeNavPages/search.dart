6c31a0e55a841d5a791349c4f02ce7d16e6edf91
BrandonDeoram <brandondeoram8931@gmail.com>
2022-05-25 22:10:36 -0400

    Started on Search feature

--- geassapp/lib/screens/homeNavPages/search.dart
+++ geassapp/lib/screens/homeNavPages/search.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Search extends StatelessWidget {
   const Search({Key? key}) : super(key: key);
@@ -6,8 +9,50 @@ class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
-    return Center(
-      child: Text('Search Anime'),
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Container(
+                    height: 150.h,
+                    width: 1000.w,
+                    child: TextField(
+                        textAlignVertical: TextAlignVertical.bottom,
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Colors.grey[700],
+                              size: 25,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 0.0),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                                width: 0.5,
+                              ),
+                            ),
+                            filled: true,
+                            hintStyle: TextStyle(
+                              color: Colors.grey[700],
+                              fontSize: 18,
+                            ),
+                            hintText: "Search",
+                            fillColor: Colors.black54))),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
